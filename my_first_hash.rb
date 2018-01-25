f938df36e94f418018085a8ba0a4679ae82419d1
Rebecca Strauss <rstrauss127@gmail.com>
2018-01-25 02:21:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,6 +6,8 @@ end
 def shipping_manifest
   the_manifest = {}
   the_manifest["whale bone corsets"] = 5
+  the_manifest["porcelain vases"] =2
+  the_manifest["oil paintings"] =3
 end
 
 def retrieval
